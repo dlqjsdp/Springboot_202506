2c965b035a5c487a2f1ec5289390c38ca5f16afe
dlqjsdp <dlqjsdp224@gmail.com>
2025-06-02 15:21:07 +0900

    first commit

---
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,27 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.ArticleForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+public class ArticleController {
+
+    @GetMapping("articles/new")
+    public String newArticleForm() {
+
+        log.info("New article form");
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm articleForm) {
+        log.info("New article created");
+        log.info("article form: {}", articleForm);
+
+        return null;
+    }
+}
